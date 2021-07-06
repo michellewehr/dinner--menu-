7d68d02198af66032f37a96873054a2988c6e684
michellewehr <michelle_wehr@icloud.com>
2021-07-05 23:38:51 -0400

    added the removeLinks function!

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-
+  function removeLinks(){
+       const links = document.getElementsByClassName('picture--links');
+       for(let i = 0; i< links.length; i++){
+           links[i].classList.remove('active');
+       }
+   }
 
    function showSelectedDinner(){
-    clearLinks();
 
-    dinners = ['Pizza','Pasta and Meatballs', 'Chicken Cutlets', 'Pork tenderloin', 'Tacos', 'Baked Chicken','Chicken Enchiladas','Hotdogs','Stirfry','Breakfast for dinner','Italian sausage','Steak','Barbeque pulled pork','Quesidillas','Speghetti Squash','Macoroni and cheese'];
-    let selectedDinner = dinners[Math.floor(Math.random()* dinners.length)];
-    
-    document.getElementById('source').innerText = selectedDinner;
-
-    showLinks();
-   };
-   
-   function showLinks(){
     dinners = ['Pizza','Pasta and Meatballs', 'Chicken Cutlets', 'Pork tenderloin', 'Tacos','Chicken Enchiladas','Hotdogs','Stirfry','Breakfast for dinner','Italian sausage','Steak','Barbeque pulled pork','Quesidillas','Speghetti Squash','Macoroni and cheese'];
     let selectedDinner = dinners[Math.floor(Math.random()* dinners.length)];
-    
+
     document.getElementById('source').innerText = selectedDinner;
+
+    removeLinks();
 
     if (selectedDinner === 'Pizza') {
         var dinnerLinks = document.getElementById('pizza');
         dinnerLinks.classList.add('active');}
 
     if (selectedDinner === 'Pasta and Meatballs') {
-            var dinnerLinks = document.getElementById('Pasta_Meatballs');
+        var dinnerLinks = document.getElementById('Pasta_Meatballs');
             dinnerLinks.classList.add('active');}
 
     if (selectedDinner === 'Chicken Cutlets') {
-            var dinnerLinks = document.getElementById('chicken_cutlets');
+        var dinnerLinks = document.getElementById('chicken_cutlets');
             dinnerLinks.classList.add('active');}
     
     if (selectedDinner === 'Pork tenderloin') {
@@ -35,8 +32,8 @@
 
 
     if (selectedDinner === 'Tacos') {
-            var dinnerLinks = document.getElementById('tacos');
-            dinnerLinks.classList.add('active');}
+        var dinnerLinks = document.getElementById('tacos');
+        dinnerLinks.classList.add('active');}
 
     if (selectedDinner === 'Chicken Enchiladas') {
         var dinnerLinks = document.getElementById('chicken_enchiladas');
@@ -51,8 +48,8 @@
         dinnerLinks.classList.add('active');}
 
      if (selectedDinner === 'Breakfast for dinner') {
-        var dinnerLinks = document.getElementById('breakfast');
-        dinnerLinks.classList.add('active');}   
+         var dinnerLinks = document.getElementById('breakfast');
+         dinnerLinks.classList.add('active');}   
         
     if (selectedDinner === 'Italian sausage') {
         var dinnerLinks = document.getElementById('italian_sausage');
@@ -76,9 +73,8 @@
 
     if (selectedDinner === 'Macoroni and cheese'){
         var dinnerLinks = document.getElementById('mac_and_cheese');
-        dinnerLinks.classList.add('active');}   
+        dinnerLinks.classList.add('active');} 
     }
-    
-    function clearLinks(){
-        var button = document.getElementById('dinner--button').remove();
-    }
+ 
+
+    document.getElementById('dinner--button').addEventListener('click', showSelectedDinner)
